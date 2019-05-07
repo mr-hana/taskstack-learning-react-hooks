6e10fb124319e8bf10309e24903aad517df873c6
hanada satoshi <owl.labo@gmail.com>
2019-05-07 22:49:06 +0900

    Updates

--- src/context/CurrentTaskContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const currentTaskInitialState = {
-  content: '',
-  duration: 0.5
-};
-export const CurrentTaskContext = React.createContext(currentTaskInitialState);
--- src/styles/theme.js
+++
@@ -1,5 +0,0 @@
-const theme = {
-  borderColor: '#666'
-};
-
-export default theme;
